bad55aede11979eaeb627b0b9988c16eda92805a
Rudy Ravelin <rudy@noukod.com>
2019-01-28 18:56:31 +0000

    Automatically backed up by Learn

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -3,41 +3,41 @@
 #   let(:article) do
 #     Post.create!(title: "Something", category: "Fiction", content: valid_content)
 #   end
-# 
+#
 #   before(:each) { visit edit_post_path(article) }
-# 
+#
 #   describe "a blank form" do
 #     it "does not render an error list" do
 #       expect(page).not_to have_selector("#error_explanation")
 #     end
-# 
+#
 #     it "does not render error fields" do
 #       expect(page).not_to have_selector(".field_with_errors")
 #     end
 #   end
-# 
+#
 #   context "invalid submissions" do
 #     let(:invalid_attributes) do
 #       { title: nil, category: "Speculative Fiction", content: "too short" }
 #     end
-# 
+#
 #     before(:each) do
 #       fill_in "Title", with: invalid_attributes[:title]
 #       fill_in "Category", with: invalid_attributes[:category]
 #       fill_in "Content", with: invalid_attributes[:content]
 #       click_button "Update"
 #     end
-# 
+#
 #     it "renders an error list" do
 #       expect(all("#error_explanation li").size).to eq(3)
 #     end
-# 
+#
 #     it "prefills fields" do
 #       expect(find("input[name=title]").value).to be_empty
 #       expect(find("input[name=category]").value).to eq(invalid_attributes[:category])
 #       expect(find("textarea[name=content]").value).to eq(invalid_attributes[:content])
 #     end
-# 
+#
 #     it "has error class on bad fields" do
 #       expect(page).to have_css(".field_with_errors input[name=title]")
 #       expect(page).to have_css(".field_with_errors input[name=category]")
@@ -45,4 +45,4 @@
 #     end
 #   end
 # end
-# 
+#
